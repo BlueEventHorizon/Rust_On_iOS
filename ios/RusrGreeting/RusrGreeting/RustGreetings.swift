24f5b311b4ac79175ecd6dcd56f5c29200b075e8
k_terada <k_terada@yumemi.co.jp>
2022-02-17 20:43:38 +0900

    initial commit

---
+++ ios/RusrGreeting/RusrGreeting/RustGreetings.swift
@@ -0,0 +1,18 @@
+//
+//  RustGreetings.swift
+//  RusrGreeting
+//
+//  Created by Katsuhiko Terada on 2022/02/17.
+//
+
+import Foundation
+
+class RustGreetings {
+    func sayHello(to: String) -> String {
+        let result = rust_greeting(to)
+        let swift_result = String(cString: result!)
+        rust_greeting_free(UnsafeMutablePointer(mutating: result))
+        return swift_result
+    }
+}
+
